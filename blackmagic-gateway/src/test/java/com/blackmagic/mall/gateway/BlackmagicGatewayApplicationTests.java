7c526493f6171254bb29a01558b63967a2fe3af0
ijolidino <ijunido@gmail.com>
2021-01-11 20:18:49 +0800

    添加网关及配置

---
+++ blackmagic-gateway/src/test/java/com/blackmagic/mall/gateway/BlackmagicGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackmagic.mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackmagicGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
